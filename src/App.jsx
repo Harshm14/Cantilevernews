9bc55885347e9ef5809f81330920c9ec6e2e3358
Harshm14 <harsh.mittal0142@gmail.com>
2024-07-09 18:09:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/common/header/Header";
+import "./App.css";
+import Homepages from "./components/home/Homepages";
+import Footer from "./components/common/footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SinglePage from "./components/singlePage/SinglePage";
+import Culture from "./components/culture/Culture";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Homepages />} />
+          <Route path='/singlepage/:id' exact element={<SinglePage />} />
+          <Route exact path='/culture' element={<Culture />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/home/Homepages.jsx
@@ -0,0 +1,17 @@
+
+import React from "react"
+import Discover from "./discover/Discover"
+import Hero from "./hero/Hero"
+import Homes from "./mainContent/homes/Home"
+
+const Homepages = () => {
+  return (
+    <>
+      <Hero />
+      <Homes />
+      <Discover />
+    </>
+  )
+}
+
+export default Homepages
